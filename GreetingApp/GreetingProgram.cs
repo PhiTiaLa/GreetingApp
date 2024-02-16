4b29e7cc8c9f2288929b077feeba4031df974c23
lagula.305871 <lagula.305871@LAB-B-PC01>
2024-02-16 09:29:38 +0800

    Add project files.

---
+++ GreetingApp/GreetingProgram.cs
@@ -0,0 +1,20 @@
+﻿namespace GreetingApp
+{
+    internal class GreetingProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+            Console.Write("Enter the total number of your enrolled courses: ");
+            int courses = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the price of your favorite book: ");
+            double fbook_Price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Total enrolled courses: " + courses);
+            Console.WriteLine("Price of my favorite book: " + fbook_Price);
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
